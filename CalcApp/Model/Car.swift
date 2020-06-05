afbafe9d2e5834c38a4566afe467a9e0a5f8d138
川向　亮 <r.kawamukai@a-sw.jp>
2020-06-05 15:09:21 +0900

    First Commit

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,27 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by ASW Kawamukai on 2020/06/05.
+//  Copyright © 2020 ASW Kawamukai. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    var frontWheel = 2
+    var rearWheel = 2
+   
+    //イニシャライズ（初期化）
+    init() {
+        frontWheel = 2
+        rearWheel = 2
+        
+    }
+    
+    func drive() {
+        print("運転開始")
+        print("前輪 \(frontWheel)")
+        print("後輪　\(rearWheel)")
+    }
+}
--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var carModel = Car()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//        carModel.frontWheel = 10
+//        carModel.rearWheel = 10
+        
     }
 
-
+    @IBAction func doAciton(_ sender: Any) {
+       
+        carModel.drive()
+        
+    }
+    
 }
 
